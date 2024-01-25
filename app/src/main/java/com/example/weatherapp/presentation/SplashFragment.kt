0d6653ae91d3903bff502a582fca481b7a102f76
Vladislav <RubashkinVlad@yandex.ru>
2024-01-25 22:47:18 +0300

    Add SplashFragment.kt and animation(MotionLayout)

---
+++ app/src/main/java/com/example/weatherapp/presentation/SplashFragment.kt
@@ -0,0 +1,63 @@
+package com.example.weatherapp.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.weatherapp.R
+import com.example.weatherapp.databinding.FragmentSplashBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? = null
+    private val binding: FragmentSplashBinding
+        get() = _binding ?: throw RuntimeException("SplashFragment == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lifecycleScope.launch {
+            delay(START_DELAY)
+            launchMainFragment()
+        }
+    }
+
+    private fun launchMainFragment() {
+        requireActivity()
+            .supportFragmentManager
+            .popBackStack()
+
+        requireActivity()
+            .supportFragmentManager
+            .beginTransaction()
+            .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+            .replace(R.id.activity_container, MainFragment.newInstance())
+            .addToBackStack(MainFragment.FRAGMENT_NAME)
+            .commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val START_DELAY = 3000L
+        const val FRAGMENT_NAME = "SplashFragment"
+
+        fun newInstance() = SplashFragment()
+    }
+}
