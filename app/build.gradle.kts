471c42edcdb1d63cd0cb3d83fe54fbdcae361566
VladislavRubashkin <RubashkinVlad@yandex.ru>
2025-04-29 12:17:26 +0300

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp) version "2.1.10-1.0.31"
+    alias(libs.plugins.androidx.navigation.safeargs)
 }
 
 android {
@@ -42,4 +44,40 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Splash screen
+    implementation(libs.androidx.core.splashscreen)
+
+    // Viewmodel scope
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Navigation
+    implementation (libs.androidx.navigation.fragment.ktx)
+    implementation (libs.androidx.navigation.ui.ktx)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
+    // Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+
+    // Dagger
+    implementation(libs.dagger)
+    ksp(libs.dagger.compiler)
+
+    // Mockk
+    testImplementation(libs.mockk)
+
+    // Junit5
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly(libs.junit.platform.launcher)
+
+    // Coroutines test
+    testImplementation (libs.kotlinx.coroutines.test)
 }
